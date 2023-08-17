6cb6c50eb848645771b59b78459fcf78102ec3c6
Alex Lopez <lex.2418@gmail.com>
2023-08-17 15:26:58 -0500

    draw image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,18 +6,31 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
   end
 
-  def build_pixel_map(%Identicon.Image{grid: grid} = _image) do
-    Enum.map(grid, fn {_code, index} ->
-      horizontal = rem(index, 5) * 50
-      vertical = div(index, 5) * 50
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
 
-      top_left = {horizontal, vertical}
-      bottom_right = {horizontal + 50, vertical + 50}
-
-      {top_left, bottom_right}
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledReactangle(image, start, stop, fill)
     end)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
